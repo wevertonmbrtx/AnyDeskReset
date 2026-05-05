b5d522a343be1388b6698967a5fd3c8a0744295d
Code_4X <wevertonmbrt@gmail.com>
2026-05-05 12:43:54 -0300

    Add timeout before initialization in AnyDesk.bat

--- AnyDesk.bat
+++ AnyDesk.bat
@@ -31,6 +31,7 @@ echo                                 -==           ===.     :=          ===.
 echo                                   ===       ===        .===      ===.
 echo                                    .===   ===:           -==-  ===-
 echo                                      .=====:               =====-
+timeout 1 >nul
 
 :init
     setlocal EnableExtensions DisableDelayedExpansion
