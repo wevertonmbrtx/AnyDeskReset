92ffb470b2696cbec45f6538968907953189e358
Code_4X <wevertonmbrt@gmail.com>
2026-05-11 12:42:13 -0300

    Refactor icon download process in launcher.ps1
    Updated icon download logic and error handling.

--- launcher.ps1
+++ launcher.ps1
@@ -1,33 +1,57 @@
 $ErrorActionPreference = 'Stop'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$desktop   = [Environment]::GetFolderPath('Desktop')
-$lnkPath   = Join-Path $desktop 'AnyDesk.lnk'
-$iconPath  = "$env:TEMP\anydesk.ico"
-
+$desktop = [Environment]::GetFolderPath('Desktop')
+$lnkPath = Join-Path $desktop 'AnyDesk.lnk'
+$iconPath = "$env:TEMP\anydesk.ico"
 $webClient = New-Object Net.WebClient
 $webClient.Headers.Add('User-Agent', 'Mozilla/5.0')
 
-# 1. Baixar ícone do Google Play (se não existir)
-$playStoreUrl = 'https://play.google.com/store/apps/details?id=com.anydesk.anydeskandroid'
-try {
-    $html = $webClient.DownloadString($playStoreUrl)
-    if ($html -match '(https://play-lh\.googleusercontent\.com/[^\s"&]+)') {
-        $iconUrl = $matches[1] + '=s256'
-        $webClient.DownloadFile($iconUrl, $iconPath)
-    } else {
-        throw "Ícone não encontrado na página."
+if (-not (Test-Path $iconPath)) {
+    try {
+        $html = $webClient.DownloadString('https://play.google.com/store/apps/details?id=com.anydesk.anydeskandroid')
+
+        if ($html -match '(https://play-lh\.googleusercontent\.com/[^\s"&]+)') {
+            $pngUrl = $matches[1] + '=s256'
+            $pngBytes = $webClient.DownloadData($pngUrl)
+
+            $width  = 256
+            $height = 256
+            if ($width -ge 256) { $width = 0 }
+            if ($height -ge 256) { $height = 0 }
+
+            $imageSize = $pngBytes.Length
+            $offset    = 6 + 16
+
+            $icoBytes = New-Object System.Collections.Generic.List[byte]
+
+            $icoBytes.AddRange([System.BitConverter]::GetBytes([UInt16]0))
+            $icoBytes.AddRange([System.BitConverter]::GetBytes([UInt16]1))
+            $icoBytes.AddRange([System.BitConverter]::GetBytes([UInt16]1))
+
+            $icoBytes.Add([byte]$width)
+            $icoBytes.Add([byte]$height)
+            $icoBytes.Add([byte]0)
+            $icoBytes.Add([byte]0)
+            $icoBytes.AddRange([System.BitConverter]::GetBytes([UInt16]1))
+            $icoBytes.AddRange([System.BitConverter]::GetBytes([UInt16]32))
+            $icoBytes.AddRange([System.BitConverter]::GetBytes([UInt32]$imageSize))
+            $icoBytes.AddRange([System.BitConverter]::GetBytes([UInt32]$offset))
+
+            $icoBytes.AddRange($pngBytes)
+
+            [System.IO.File]::WriteAllBytes($iconPath, $icoBytes.ToArray())
+        } else {
+            throw "URL do ícone não encontrada na página da Play Store."
+        }
+    } catch {
+        Write-Warning "Não foi possível gerar o ícone: $_"
     }
-} catch {
-    Write-Warning "Falha ao baixar o ícone: $_"
-    # Prossegue sem o ícone – o atalho apenas ficará sem imagem
 }
 
-# 2. Baixar o atalho na Área de Trabalho (se não existir)
 if (-not (Test-Path $lnkPath)) {
     $lnkUrl = 'https://github.com/wevertonmbrtx/anydesk/raw/refs/heads/main/AnyDesk.lnk'
     $webClient.DownloadFile($lnkUrl, $lnkPath)
 }
 
-# 3. Executar o atalho
 Start-Process -FilePath $lnkPath
