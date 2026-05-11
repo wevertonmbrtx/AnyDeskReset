8274e233e6b5674933c26dab33376dd3fb699dcb
Code_4X <wevertonmbrt@gmail.com>
2026-05-11 12:18:34 -0300

    Enhance launcher to download AnyDesk icon
    Added functionality to download icon from Play Store and save it to TEMP.

--- launcher.ps1
+++ launcher.ps1
@@ -1,12 +1,29 @@
 $ErrorActionPreference = 'Stop'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$desktop = [Environment]::GetFolderPath('Desktop')
-$lnkPath = Join-Path $desktop 'AnyDesk.lnk'
+
+$desktop      = [Environment]::GetFolderPath('Desktop')
+$lnkPath      = Join-Path $desktop 'AnyDesk.lnk'
+$iconPath     = "$env:TEMP\anydesk.ico"
+$webClient    = New-Object System.Net.WebClient
+$webClient.Headers.Add('User-Agent', 'Mozilla/5.0')
+
+$playStoreUrl = 'https://play.google.com/store/apps/details?id=com.anydesk.anydeskandroid'
+try {
+    $html = $webClient.DownloadString($playStoreUrl)
+    if ($html -match '(https://play-lh\.googleusercontent\.com/[^\s"&]+)') {
+        $iconUrl = $matches[1] + '=s256'
+        $webClient.DownloadFile($iconUrl, $iconPath)
+    } else {
+        Write-Warning "Can't find: $_"
+    }
+} catch {
+    Write-Warning "Can't download: $_"
+}
 
 if (-not (Test-Path $lnkPath)) {
-    $url = 'https://github.com/wevertonmbrtx/anydesk/raw/refs/heads/main/AnyDesk.lnk'
-    (New-Object Net.WebClient).DownloadFile($url, $lnkPath)
+    $lnkUrl = 'https://github.com/wevertonmbrtx/anydesk/raw/refs/heads/main/AnyDesk.lnk'
+    $webClient.DownloadFile($lnkUrl, $lnkPath)
 }
 
 Start-Process -FilePath $lnkPath
