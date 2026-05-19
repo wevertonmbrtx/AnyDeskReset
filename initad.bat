a785eba21530d91c2186fe853d35444d2aedb55b
Code_4X <wevertonmbrt@gmail.com>
2026-05-18 23:42:08 -0300

    Remove colon from goto statement in initad.bat

--- initad.bat
+++ initad.bat
@@ -159,7 +159,7 @@ if exist "%insPath0%" (
     " $lnkUrl = 'https://raw.githubusercontent.com/wevertonmbrtx/anydesk/refs/heads/main/AnyDesk.lnk';" ^
     " $webClient.DownloadFile($lnkUrl, $lnkPath);" ^
     "}"
-    goto :main
+    goto main
 )  
     echo Success.
     goto :eof
